ba06011080dd95ca6a67beda21344052d8fe6405
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-02-09 11:56:19 +0200

    Add files via upload

---
+++ Mobile-app/Mobile app/Add To Trolley.js
@@ -0,0 +1,69 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://realtime-database-c755b-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const groceryListInDB = ref(database, "groceryList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const groceryListEl = document.getElementById("grocery-list");
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value;
+    push(groceryListInDB, inputValue);
+    clearInputFieldEl()
+});
+
+onValue(groceryListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val());
+        clearGroceryListEl();
+
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i];
+            let currentItemID = currentItem[0];
+            let currentItemValue = currentItem[1];
+            appendItemToGrocerylistEl(currentItem);
+        }
+    } else {
+        groceryListEl.innerHTML = "No things yet";
+    }
+});
+
+function clearGroceryListEl() {
+    groceryListEl.innerHTML = "";  
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = "";
+}  
+
+function appendItemToGrocerylistEl(currentItem) {
+    let itemID = currentItem[0];
+    let itemValue = currentItem[1];
+    
+    let newEl = document.createElement("li");
+    newEl.textContent = itemValue;
+
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `groceryList/${itemID}`);
+        remove(exactLocationOfItemInDB);
+    });
+
+    groceryListEl.appendChild(newEl);
+}
+
+// Get the user's name
+const name = prompt("what is your name?");
+
+//  welcome message
+const welcomeMessage = `Welcome , ${name}! `;
+
+//Display welcome messsage
+alert(welcome);
